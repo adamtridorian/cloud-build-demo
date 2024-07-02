9dcc1d67f7b35822e9d512e2119549131d303d57
Adam Morgan <adammorgan@Adams-Air.ST-244F>
2024-07-02 13:31:00 +0700

    intract

--- startup.sh
+++ startup.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 cd /home/adam_boonchaya/cloud-build-demo
 sudo ssh-keyscan -t rsa github.com >> ~/.ssh/known_hosts
+sudo git config --global --add safe.directory /home/adam_boonchaya/cloud-build-demo
 sudo git pull
 sudo cp -r * /var/www/html
 sudo rm -rf /var/www/html/.git
